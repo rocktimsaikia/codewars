415dbfe63b151aadbfc15cc8aad8d1cc0b26b209
rocktimsaikia <rocktimthedev@gmail.com>
2024-09-15 19:38:57 +0530

    more completed ones

---
+++ 8-kyu/c/is-even.c
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+#include <math.h>
+
+bool is_even(double n) {
+	return fmod(n, 2) == 0;
+}
---
+++ 8-kyu/c/remove-every-other.c
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+void remove_every_other(size_t *length, int array[*length]) {
+	size_t new_length = 0;
+	for (size_t i=0; i< *length; i+=2) {
+		array[new_length] = array[i];
+		new_length++;
+	}
+	*length = new_length;
+}
